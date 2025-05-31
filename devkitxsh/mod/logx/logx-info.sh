9053531e9f31a1de489a6e944012f3f6f6562e32
Davide Di Criscito <davide.dicriscito@gmail.com>
2025-05-31 05:53:29 +0200

    refactor: translate logx-info to en

--- devkitxsh/mod/logx/logx-info.sh
+++ devkitxsh/mod/logx/logx-info.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
-# logx-info.sh - Funzione per messaggi informativi (logx)
+# File: devkitxsh/mod/logx/logx-info.sh
+# Location: $DEVKITX_REPO/devkitxsh/mod/logx/logx-info.sh
+#
+# Description:
+#   Defines the 'logx::info' function for the DevKitXsh 'logx' module.
+#   This function is responsible for displaying informational messages.
+#   It supports plain mode (no emoji) and respects global emoji settings.
+#   Informational messages are output to stdout.
+#   This function can be suppressed if the LOGX_QUIET environment variable is set to "true".
+#
+# Usage:
+#   This script is intended to be sourced by other scripts within the 'logx' module.
+#   Example:
+#     source "path/to/logx-info.sh"
+#     logx::info "This is an informational update."
+#
+# Functions:
+#   logx::info <message> - Displays an informational message to stdout.
+#
+# Environment Variables:
+#   LOGX_QUIET      If set to "true", the informational message will be suppressed.
+#   LOGX_PLAIN      If set to "true", emoji output will be disabled for this message.
+#   DEVKITX_EMOJI   If set to "false", emoji output will be disabled globally (and for this message).
 
 set -euo pipefail
 
